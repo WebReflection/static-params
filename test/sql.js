7d3585026537dd30f5dbeab57f885bbe5a120d4d
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2022-01-18 15:22:38 +0100

    better tests around sql variant

--- test/sql.js
+++ test/sql.js
@@ -4,7 +4,14 @@ const tag = asTag((template, ...values) => {
   return [template, values];
 });
 
-const [template, values] = tag`SELECT * FROM ${asStatic('table')} WHERE id IN (${[1, 2, 3]}) OR name = ${'test'}`;
-
+let [template, values] = tag`SELECT * FROM ${asStatic('table')} WHERE id IN (${[1, 2, 3]}) OR name = ${'test'}`;
 console.assert(template.join('?') === 'SELECT * FROM table WHERE id IN (?,?,?) OR name = ?');
 console.assert(JSON.stringify(values) === '[1,2,3,"test"]');
+
+[template, values] = tag`SELECT * FROM ${asStatic('table')} WHERE id IN (${[4]}) OR name = ${'test'}`;
+console.assert(template.join('?') === 'SELECT * FROM table WHERE id IN (?) OR name = ?');
+console.assert(JSON.stringify(values) === '[4,"test"]');
+
+[template, values] = tag`SELECT * FROM ${asStatic('table')} WHERE id IN (${[5,6]}) OR name IN (${['A', 'B']})`;
+console.assert(template.join('?') === 'SELECT * FROM table WHERE id IN (?,?) OR name IN (?,?)');
+console.assert(JSON.stringify(values) === '[5,6,"A","B"]');
