5ab48b790a28a417faf378d2cd86f4763cb58bb9
webreflection <andrea.giammarchi@gmail.com>
2023-10-23 12:07:29 +0200

    Updating package minor due changes that won't throw anymore on invalid templates

--- index.js
+++ index.js
@@ -2,49 +2,40 @@ self.staticParams = (function (exports) {
   'use strict';
 
   var cache = new WeakMap();
-
   var asTag = function asTag(fn) {
     return function () {
       return fn.apply(this, asParams.apply(null, arguments));
     };
   };
-
   var asParams = function asParams(template) {
     for (var _len = arguments.length, values = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
       values[_key - 1] = arguments[_key];
     }
-
     var _parse = parse(template, values),
-        t = _parse.t,
-        v = _parse.v;
-
+      t = _parse.t,
+      v = _parse.v;
     var parsed = cache.get(template) || cache.set(template, {}).get(template);
     return (parsed[t] || (parsed[t] = [t])).concat(v.map(function (i) {
       return values[i];
     }));
   };
-
   var parse = function parse(template, values) {
     var t = [template[0]];
     var v = [];
-
     for (var c = 0, i = 0, j = 0, length = values.length; i < length; i++) {
       if (values[i] instanceof Static) t[c] += values[i].v + template[i + 1];else {
         v[j++] = i;
         t[++c] = template[i + 1];
       }
     }
-
     return {
       t: t,
       v: v
     };
   };
-
   var asStatic = function asStatic(value) {
     return new Static(value);
   };
-
   function Static(v) {
     this.v = v;
   }
@@ -53,6 +44,8 @@ self.staticParams = (function (exports) {
   exports.asStatic = asStatic;
   exports.asTag = asTag;
 
+  Object.defineProperty(exports, '__esModule', { value: true });
+
   return exports;
 
-}({}));
+})({});
